038fefe7860b960174be5ac2ab3d800d0131bd0e
SUPA93 <supaflavaz93@hotmail.com>
2023-06-28 14:58:32 +0200

    code update

---
+++ Product.js
@@ -0,0 +1,13 @@
+//Constructor for new entry off used vehicule
+export default class UsedVehicule{
+    constructor(marque, model, metrage, price, color, firsthand, location){
+        this.marque = marque,
+        this.model = model,
+        this.metrage = metrage,
+        this.price = price,
+        this.color = color,
+        this.firsthand = firsthand,
+        this.location = location
+
+    }
+}
