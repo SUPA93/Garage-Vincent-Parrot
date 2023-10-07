19f4d35ca9b2f2fcbcf628e53f42aeca8805ff2a
SUPA93 <supaflavaz93@hotmail.com>
2023-10-08 00:51:15 +0200

    changed card gap

--- scripts/caroussel.js
+++ scripts/caroussel.js
@@ -1,27 +1,27 @@
 let carousel = document.querySelector('.carousel');
-let moving = false;  // pour savoir si une transition est en cours
+let moving = false;  
 
 function moveCarousel() {
-    if (moving) return;  // s'il y a une transition en cours, ne faites rien
+    if (moving) return;  
 
     let firstChild = carousel.firstElementChild;
-    let width = firstChild.clientWidth + 20;  // ajoutez l'écart entre les cartes
+    let width = firstChild.clientWidth + 20;  // ne pas oublier de changer la taille du gap
 
-    moving = true;  // indique qu'une transition est en cours
-    carousel.style.transition = 'transform 3s linear';
+    moving = true;  // transition en cours
+    carousel.style.transition = 'transform 3s linear';//correspond au CSS
     carousel.style.transform = `translateX(-${width}px)`;
 
-    // Cette fonction sera appelée une fois la transition terminée
+    // fonction appelée une fois la transition terminée
     carousel.addEventListener('transitionend', function () {
-        carousel.style.transition = 'none';  // désactive la transition
-        carousel.style.transform = 'translateX(0)';  // remet à zéro la position du carrousel sans transition
+        carousel.style.transition = 'none';  // désactive 
+        carousel.style.transform = 'translateX(0)';  // remet à zéro 
         carousel.removeChild(firstChild);  // supprime la première carte
-        carousel.appendChild(firstChild);  // ajoute la carte à la fin du carrousel
-        moving = false;  // indique que la transition est terminée
+        carousel.appendChild(firstChild);  // ajoute la première carte à la fin du carrousel
+        moving = false;  // transition terminée
 
-        requestAnimationFrame(moveCarousel);  // planifie le prochain déplacement
-    }, { once: true });  // garantit que cet écouteur d'événement est exécuté une seule fois
+        requestAnimationFrame(moveCarousel);  // prochain déplacement
+    }, { once: true });  // écouteur d'événement exécuté une seule fois
 }
 
-// Commencez le défilement
+// appel
 moveCarousel();
