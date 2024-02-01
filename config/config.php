98b15488456095ee31b5b112437d1824a61e1189
SUPA93 <91850048+SUPA93@users.noreply.github.com>
2024-02-01 11:55:23 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -9,15 +9,7 @@ ini_set('display_errors', 1); */
 // Informations  base de données
 //EN LIGNE
 
-/* $dbHost = "localhost";
-$dbName = "mfkrffbm_vparrot";
-$dbUser = "mfkrffbm_adminadmin";
-$dbPassword = "Bilallauis93350@"; */
-//EN LOCAL
-$dbHost = "localhost";
-$dbName = "vparrot";
-$dbUser = "root";
-$dbPassword = "";
+
 try {
     // connexion à la base de données avec PDO
     $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
