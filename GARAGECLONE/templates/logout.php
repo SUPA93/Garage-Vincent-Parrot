65a547267250949578405549b616b1afdbfb9f78
SUPA93 <supaflavaz93@hotmail.com>
2024-02-01 10:34:19 +0100

    FLushed OLD FILES

--- GARAGECLONE/templates/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-//Fonction de déconnexion
-session_start();
-session_destroy();
-session_unset();
-header('Location: connexion.php');
-exit();
-
-?>
--- GARAGECLONE/templates/test_session.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-    echo "Connecté en tant que " . $_SESSION["user"]["name"];
-} else {
-    echo "Non connecté.";
-}
