6ef4f0a53488c438772d969eab16d3ced8217f0e
SUPA93 <supaflavaz93@hotmail.com>
2024-02-26 21:37:10 +0100

    nothing much

--- lib/carsFilter.php
+++ lib/carsFilter.php
@@ -3,12 +3,12 @@ include_once __DIR__ . '/pdo.php';
 $pdo = connectToDatabase();
 /* var_dump($_GET); */
 // Récupération des paramètres GET et nettoyage
-$priceMin = isset($_GET['priceMin']) ? intval($_GET['priceMin']) : '';
-$priceMax = isset($_GET['priceMax']) ? intval($_GET['priceMax']) : '';
-$mileageMin = isset($_GET['mileageMin']) ? intval($_GET['mileageMin']) : '';
-$mileageMax = isset($_GET['mileageMax']) ? intval($_GET['mileageMax']) : '';
-$yearMin = isset($_GET['yearMin']) ? intval($_GET['yearMin']) : '';
-$yearMax = isset($_GET['yearMax']) ? intval($_GET['yearMax']) : '';
+$priceMin = isset($_GET['priceMin']) ? intval($_GET['priceMin'], ENT_QUOTES, 'UTF-8') : '';
+$priceMax = isset($_GET['priceMax']) ? intval($_GET['priceMax'], ENT_QUOTES, 'UTF-8') : '';
+$mileageMin = isset($_GET['mileageMin']) ? intval($_GET['mileageMin'], ENT_QUOTES, 'UTF-8') : '';
+$mileageMax = isset($_GET['mileageMax']) ? intval($_GET['mileageMax'], ENT_QUOTES, 'UTF-8') : '';
+$yearMin = isset($_GET['yearMin']) ? intval($_GET['yearMin'], ENT_QUOTES, 'UTF-8') : '';
+$yearMax = isset($_GET['yearMax']) ? intval($_GET['yearMax'], ENT_QUOTES, 'UTF-8') : '';
 $sql = "SELECT * FROM used_vehicules WHERE 1=1";
 
 if (!empty($_GET['priceMin'])) {
